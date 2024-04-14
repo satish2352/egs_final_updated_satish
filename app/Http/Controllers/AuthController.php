c412db603895ec6c6b91eac9c1971470c2a38f82
Satish <satish@sumagoinfotech.com>
2024-04-14 10:47:57 +0530

    changes for the auth controller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
+use Validator;
+use Illuminate\Http\Request;
+use App\Models\ {
+	User
+};
 
 class AuthController extends Controller
 {
@@ -22,15 +27,60 @@ class AuthController extends Controller
      *
      * @return \Illuminate\Http\JsonResponse
      */
-    public function login()
+    public function login(Request $request)
     {
+
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required',
+            'device_id'=>'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['status' => 'error', 'message' => $validator->errors()->all()], 200);
+        }
+
+
+        $email = $request->email;
+        $password = $request->password;
+        $device_id = $request->device_id;
+
+        $user = User::where('email', $email)->first();
+        if (!$user) {
+            return response()->json(['status' => 'False','message' => 'User not found'], 200);
+        }
+
+        if ($user->device_id != 'null' && $user->device_id != $device_id) {
+            return response()->json(['status' => 'False', 'message' => 'This user is associated with another device please login with same'], 200);
+        }
+
+        if ($user->device_id== 'null') {
+            User::where('email', $email)->update(['device_id' => $device_id]);
+        }
+
         $credentials = request(['email', 'password']);
 
         if (! $token = auth()->attempt($credentials)) {
             return response()->json(['error' => 'Unauthorized'], 401);
         }
 
-        return $this->respondWithToken($token);
+        if ($user->device_id== 'null') {
+            User::where('email', $email)->update(['device_id' => $device_id]);
+        }
+
+        $user->update(['remember_token' => $token]);
+
+
+        return response()->json([
+            'status' => 'True',
+            'message' => 'Login successfully',
+            'data' => $user,
+            'token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60
+            // 'expires_in' => auth()->factory()->getTTL() * 60 * 24 * 365 * 10, // 10 years
+        ]);
+
     }
 
     /**
@@ -65,19 +115,5 @@ class AuthController extends Controller
         return $this->respondWithToken(auth()->refresh());
     }
 
-    /**
-     * Get the token array structure.
-     *
-     * @param  string $token
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
-    protected function respondWithToken($token)
-    {
-        return response()->json([
-            'access_token' => $token,
-            'token_type' => 'bearer',
-            'expires_in' => auth()->factory()->getTTL() * 60
-        ]);
-    }
+   
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,12 +12,35 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+
+            $table->bigIncrements('id');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            // $table->string('u_uname');
             $table->string('password');
+            $table->unsignedBigInteger('role_id');
+            $table->string('f_name');
+            $table->string('m_name');
+            $table->string('l_name');
+            $table->string('number');
+            $table->string('device_id')->default('null');
+            $table->string('aadhar_no');
+            $table->string('address');
+            $table->string('state');
+            $table->string('district');
+            $table->string('taluka');
+            $table->string('village');
+            $table->string('pincode');
+            $table->string('user_type');
+            $table->string('user_district');
+            $table->string('user_taluka');
+            $table->string('user_village');
+            $table->string('ip_address');
+            $table->string('user_agent')->default('null');
+            $table->string('user_profile')->default('null');
             $table->rememberToken();
+            // $table->text('remember_token', 255)->nullable();
+            $table->boolean('is_active')->default(true);
+            // $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
             $table->timestamps();
         });
     }
