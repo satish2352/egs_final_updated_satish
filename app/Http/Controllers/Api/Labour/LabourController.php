6402c4af05cb391793d155e5e28823da4a0c3400
Savita444 <savita.hajare@sumagoinfotech.com>
2024-04-17 16:41:16 +0530

    change in labour list api

--- app/Http/Controllers/Api/Labour/LabourController.php
+++ app/Http/Controllers/Api/Labour/LabourController.php
@@ -265,7 +265,7 @@ class LabourController extends Controller
             if (sizeof($data_output) > 0) {
                 $totalPages = ceil($totalRecords/$rowperpage);
             } else {
-                $totalPages = 1;
+                $totalPages = 0;
             }
 
             return response()->json(['status' => 'true', 'message' => 'All data retrieved successfully', "totalRecords" => $totalRecords, "totalPages"=>$totalPages, 'page_no_to_hilight'=>$page, 'data' => $data_output], 200);
