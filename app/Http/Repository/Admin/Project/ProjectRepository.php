e3cf13a809ec943fb8285a36b361cf6838bf2dbe
mhaskemahesh <maheshmhaske500@gmail.com>
2024-05-14 17:33:55 +0530

    project add issue resolved

--- app/Http/Repository/Admin/Project/ProjectRepository.php
+++ app/Http/Repository/Admin/Project/ProjectRepository.php
@@ -106,7 +106,6 @@ class ProjectRepository
 		try {
 		$data =array();
 		$project_data = new Project();
-		dd($project_data);
 		$project_data->project_name = $request['project_name'];
 		$project_data->state = '';
 		$project_data->district = $request['district'];
