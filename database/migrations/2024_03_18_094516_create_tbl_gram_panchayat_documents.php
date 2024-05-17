cb832df2826ee44777850e2fef62c7e805505526
Savita444 <savita.hajare@sumagoinfotech.com>
2024-05-17 10:10:59 +0530

    changes

--- database/migrations/2024_03_18_094516_create_tbl_gram_panchayat_documents.php
+++ database/migrations/2024_03_18_094516_create_tbl_gram_panchayat_documents.php
@@ -13,15 +13,15 @@ return new class extends Migration
     {
         Schema::create('tbl_gram_panchayat_documents', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('user_id');
-            $table->unsignedBigInteger('document_type_id');
+            $table->integer('user_id');
+            $table->integer('document_type_id');
             $table->string('document_name');
             $table->string('latitude');
             $table->string('longitude');
             $table->string('document_pdf')->default('null');
-            $table->boolean('is_approved')->default(true);
-            $table->boolean('is_resubmitted')->default(false);
-            $table->unsignedBigInteger('reason_doc_id')->nullable();
+            $table->integer('is_approved')->default(true);
+            $table->integer('is_resubmitted')->default(false);
+            $table->integer('reason_doc_id')->nullable();
             $table->string('other_remark')->nullable();
             $table->integer('is_active')->default(true);
             $table->integer('is_deleted')->default(false);
