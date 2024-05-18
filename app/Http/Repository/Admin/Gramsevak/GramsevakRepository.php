4d7bfcb5cdda4740a77be4e8eddb1c0a7fde9b94
mhaskemahesh <maheshmhaske500@gmail.com>
2024-05-18 18:49:46 +0530

    gramsevak doc resubmitted issue

--- app/Http/Repository/Admin/Gramsevak/GramsevakRepository.php
+++ app/Http/Repository/Admin/Gramsevak/GramsevakRepository.php
@@ -744,28 +744,7 @@ class GramsevakRepository
             $utype=$data_output->user_type;
             $user_working_dist=$data_output->user_district;
             $user_working_tal=$data_output->user_taluka;
-            $user_working_vil=$data_output->user_village;
-
-
-            // if($utype=='1')
-            // {
-            // $data_user_output = User::where('users.user_district', $user_working_dist)
-            // ->select('id')
-            //     ->get()
-			// 	->toArray();
-            // }else if($utype=='2')
-            // {
-            //     $data_user_output = User::where('users.user_taluka', $user_working_tal)
-            //     ->select('id')
-            //     ->get()
-			// 	->toArray();
-            // }else if($utype=='3')
-            // {
-            //     $data_user_output = User::where('users.user_village', $user_working_vil)
-            //     ->select('id')
-            //     ->get()
-			// 	->toArray();
-            // }         
+            $user_working_vil=$data_output->user_village;    
 
 
 			$data_users = User::leftJoin('roles', 'roles.id', '=', 'users.role_id')
@@ -787,7 +766,7 @@ class GramsevakRepository
                 ->get()
 				->toArray();
 
-		$data_output = User::leftJoin('usertype', 'users.user_type', '=', 'usertype.id')
+			$data_output = User::leftJoin('usertype', 'users.user_type', '=', 'usertype.id')
                 ->where('users.id', $sess_user_id)
                 ->first();
 
@@ -824,6 +803,23 @@ class GramsevakRepository
 				->leftJoin('tbl_area as village_user', 'users.user_village', '=', 'village_user.location_id')
 				->where('users.role_id','3')
                 ->whereIn('users.id',$data_user_output)
+                ->whereIn('users.id',$doc_output)
+				->select('users.id','users.f_name','users.m_name','users.l_name','users.email','users.number','users.aadhar_no',
+				'users.address','users.pincode','users.user_profile','roles.role_name',
+				'district_user.name as district','taluka_user.name as taluka','village_user.name as village')
+				->get();
+		}
+		else if($sess_user_role=='3')
+		{
+			 
+
+
+			$data_users = User::leftJoin('roles', 'roles.id', '=', 'users.role_id')
+				->leftJoin('tbl_area as district_user', 'users.user_district', '=', 'district_user.location_id')
+				->leftJoin('tbl_area as taluka_user', 'users.user_taluka', '=', 'taluka_user.location_id')
+				->leftJoin('tbl_area as village_user', 'users.user_village', '=', 'village_user.location_id')
+				->where('users.role_id','3')
+                ->where('users.id',$sess_user_id)
                 ->whereIn('users.id',$doc_output)
 				->select('users.id','users.f_name','users.m_name','users.l_name','users.email','users.number','users.aadhar_no',
 				'users.address','users.pincode','users.user_profile','roles.role_name',
