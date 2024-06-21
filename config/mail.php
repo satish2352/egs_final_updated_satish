323a8daa164af12ce947c568aca61869424a68a9
mhaskemahesh <maheshmhaske500@gmail.com>
2024-06-21 11:45:03 +0530

    for forgot password

--- config/mail.php
+++ config/mail.php
@@ -48,9 +48,9 @@ return [
         'smtp' => [
             'transport' => 'smtp',
             'url' => env('MAIL_URL'),
-            'host' => env('MAIL_HOST', 'mail.nicecomp.co.in'),
+            'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
             'port' => env('MAIL_PORT', 587),
-            'encryption' => env('MAIL_ENCRYPTION', null),
+            'encryption' => env('MAIL_ENCRYPTION', 'tls'),
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
