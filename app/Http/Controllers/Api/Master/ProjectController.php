b8bccd4034a61ca6c679d0b7709f7b939a3f301f
mhaskemahesh <maheshmhaske500@gmail.com>
2024-07-27 11:39:41 +0530

    project not showing

--- app/Http/Controllers/Api/Master/ProjectController.php
+++ app/Http/Controllers/Api/Master/ProjectController.php
@@ -68,20 +68,36 @@ class ProjectController extends Controller
     private function calculateDistance($lat1, $lon1, $lat2, $lon2)
     {
         $R = 6371; // Radius of the Earth in kilometers
-        $lat1 = (float) $lat1;
-        $lon1 = (float) $lon1;
+        
+        // $lat1 = (float) $lat1;
+        // $lon1 = (float) $lon1;
 
-        $lat2 = (float) $lat2;
-        $lon2 = (float) $lon2;
-        $dLat = $this->degreesToRadians($lat2 - $lat1);
-        $dLon = $this->degreesToRadians($lon2 - $lon1);
+        // $lat2 = (float) $lat2;
+        // $lon2 = (float) $lon2;
+        // $dLat = $this->degreesToRadians($lat2 - $lat1);
+        // $dLon = $this->degreesToRadians($lon2 - $lon1);
 
-        $a = sin($dLat / 2) ** 2 +
-             cos($this->degreesToRadians($lat1)) * cos($this->degreesToRadians($lat2)) * sin($dLon / 2) ** 2;
+        // $a = sin($dLat / 2) ** 2 +
+        //      cos($this->degreesToRadians($lat1)) * cos($this->degreesToRadians($lat2)) * sin($dLon / 2) ** 2;
 
-        $c = 2 * atan2(sqrt($a), sqrt(1 - $a));
+        // $c = 2 * atan2(sqrt($a), sqrt(1 - $a));
 
-        return $R * $c; // Distance in kilometers
+        // return $R * $c; // Distance in kilometers
+
+    $latFrom = deg2rad($lat1);
+    $lonFrom = deg2rad($lon1);
+    $latTo = deg2rad($lat2);
+    $lonTo = deg2rad($lon2);
+
+    $latDelta = $latTo - $latFrom;
+    $lonDelta = $lonTo - $lonFrom;
+
+    $angle = 2 * asin(sqrt(pow(sin($latDelta / 2), 2) +
+      cos($latFrom) * cos($latTo) * pow(sin($lonDelta / 2), 2)));
+      Log::info($angle * $R);
+
+
+    return $angle * $R;
     }
 
     // Convert degrees to radians
